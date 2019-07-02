39bcbe4d0b1f02dc0c320643d3e3430fd5dfe2ba
Ben R-W <ben.romero-wilcock17@imperial.ac.uk>
2019-07-02 15:31:16 +0100

    put javascript file in a window onload function

--- scripts/journey.js
+++ scripts/journey.js
@@ -4,16 +4,21 @@ let vis_pg = document.getElementsByClassName("vis_pg");
 let i;
 let scrolling;
 let pg_heights=[];
+let pg_heights2 = [];
 let h;
 
+window.onload = function() {
 for (i=0; i<journey_pg.length; i++) {
-    console.log(journey_pg)
     //console.log(journey_pgs)
+    pg_heights2.push($("#"+journey_pg[i].id).offset().top);
     h=0
     for (j=0; j<(i+1); j++) {
-        h+=parseFloat(journey_pg[j].scrollHeight)
+        //h+=parseFloat(journey_pg[j].scrollHeight)
+        h+=$("#"+journey_pg[j].id).outerHeight()
     }
     pg_heights.push(h)
+    console.log(pg_heights)
+    console.log(pg_heights2)
     if (i%2===1) {
         journey_pg[i].style = "background-color: #e3eae6";
     } else {
@@ -40,3 +45,4 @@ let app = new Vue({
         document.removeEventListener('scroll', this.test)
     }
 })
+}
