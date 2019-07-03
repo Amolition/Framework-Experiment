da965ef3b3caf9fe2e906ba84400f4d29e83f6b4
Unknown <av5218@ic.ac.uk>
2019-07-03 15:07:38 +0100

    Antoine commit

---
+++ Fourier Test/README.md
@@ -0,0 +1,2 @@
+# Framework-Experiment
+Dedicated repository for testing how to build a template of the scrolling framework.
--- Fourier Test/scripts/journey.js
+++ Fourier Test/scripts/journey.js
@@ -8,14 +8,6 @@ let pg_heights=[];
 let pg_heights2 = [];
 let h;
 
-
-//let BigBox = new Vue({
-//    el: '#BigBox',
-//    data: {
- //       currentSection: 1,
- //   },
-//})
-
 window.onload = function() {
 for (i=0; i<journey_pg.length; i++) {
     //console.log(journey_pgs)
@@ -57,9 +49,6 @@ let app = new Vue({
 }
 
 window.onresize = function() {
-    //location.reload(); This works but reloading the page is not ideal.
-    pg_heights = [];
-    pg_heights2 = [];
     for (i=0; i<journey_pg.length; i++) {
         //console.log(journey_pgs)
         h=0;
@@ -69,7 +58,12 @@ window.onresize = function() {
         }
         pg_heights2.push(h)
         console.log(pg_heights2)
-        
+        if (i%2===1) {
+            journey_pg[i].style = "background-color: #e3eae6";
+        } else {
+            //journey_pg[i].style = "background-color: #f7f7f7";
+            journey_pg[i].style = "background-color:  #878787";
+        }
     }
 
     app.pg_heights = pg_heights2;
