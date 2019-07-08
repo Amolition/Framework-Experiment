f31479a28d0466383e299b458cde890810e4cf87
Ben R-W <ben.romero-wilcock17@imperial.ac.uk>
2019-07-08 11:49:11 +0100

    integrated progress bar in vue-based framework

--- scripts/journey.js
+++ scripts/journey.js
@@ -1,48 +1,77 @@
 let journey_pg = document.getElementsByClassName("journey_pg");
+let progress_buttons = document.getElementsByClassName("progress_button")
+let journey_cont = document.getElementById("journey_cont")
 //let journey_pgs = $(".journey_pg").height();
 let vis_pg = document.getElementsByClassName("vis_pg");
 let i;
 let scrolling;
-let pg_heights=[];
+let pg_heights = [];
 let pg_heights2 = [];
 let h;
 
-window.onload = function() {
-for (i=0; i<journey_pg.length; i++) {
-    //console.log(journey_pgs)
-    pg_heights2.push($("#"+journey_pg[i].id).offset().top);
-    h=0
-    for (j=0; j<(i+1); j++) {
-        //h+=parseFloat(journey_pg[j].scrollHeight)
-        h+=$("#"+journey_pg[j].id).outerHeight()
-    }
-    pg_heights.push(h)
-    console.log(pg_heights)
-    console.log(pg_heights2)
-    if (i%2===1) {
-        journey_pg[i].style = "background-color: #e3eae6";
-    } else {
-        //journey_pg[i].style = "background-color: #f7f7f7";
-        journey_pg[i].style = "background-color:  #878787"
-    }
-}
+window.onload = function () {
+    progress_buttons[0].style = "background-color: #aa3758; color: white;"
 
-let app = new Vue({
-    el: "#app",
-    data: {
-        pg_heights: pg_heights,
-        scrolling: 0,
-    },
-    methods: {
-        OnScroll: function(event){
-            this.scrolling = parseFloat($(".container_journey").scrollTop());
+    for (i = 0; i < journey_pg.length; i++) {
+        //console.log(journey_pgs)
+        pg_heights2.push($("#" + journey_pg[i].id).offset().top);
+        h = 0
+        for (j = 0; j < (i + 1); j++) {
+            //h+=parseFloat(journey_pg[j].scrollHeight)
+            h += $("#" + journey_pg[j].id).outerHeight()
         }
-    },
-    mounted: function() {
-        document.addEventListener('scroll', this.OnScroll)
-      },
-    destroyed: function() {
-        document.removeEventListener('scroll', this.test)
+        pg_heights.push(h)
     }
-})
+
+    let app = new Vue({
+        el: "#app",
+        data: {
+            pg_heights: pg_heights,
+            scrolling: 0,
+        },
+        methods: {
+            OnScroll: function (event) {
+                this.scrolling = parseFloat($(".container_journey").scrollTop());
+                if (this.scrolling < pg_heights[0]) {
+                    progress_buttons[0].style = "background-color: #aa3758; color: white;";
+                    progress_buttons[1].style = "background-color: white; color: black;";
+                    progress_buttons[2].style = "background-color: white; color: black;";
+                    progress_buttons[3].style = "background-color: white; color: black;";
+                } else if (this.scrolling<pg_heights[1]) {
+                    progress_buttons[0].style = "background-color: white; color: black;";
+                    progress_buttons[1].style = "background-color: #aa3758; color: white;";
+                    progress_buttons[2].style = "background-color: white; color: black;";
+                    progress_buttons[3].style = "background-color: white; color: black;";
+                } else if (this.scrolling<pg_heights[2]) {
+                    progress_buttons[0].style = "background-color: white; color: black;";
+                    progress_buttons[1].style = "background-color: white; color: black;";
+                    progress_buttons[2].style = "background-color: #aa3758; color: white;";
+                    progress_buttons[3].style = "background-color: white; color: black;";
+                } else if (this.scrolling<pg_heights[3]) {
+                    progress_buttons[0].style = "background-color: white; color: black;";
+                    progress_buttons[1].style = "background-color: white; color: black;";
+                    progress_buttons[2].style = "background-color: white; color: black;";
+                    progress_buttons[3].style = "background-color: #aa3758; color: white;";
+                }
+            },
+            Scroll1: function (event) {
+                journey_pg[0].scrollIntoView({behavior:"smooth"})
+            },
+            Scroll2: function (event) {
+                journey_pg[1].scrollIntoView({behavior:"smooth"})
+            },
+            Scroll3: function (event) {
+                journey_pg[2].scrollIntoView({behavior:"smooth"})
+            },
+            Scroll4: function (event) {
+                journey_pg[3].scrollIntoView({behavior:"smooth"})
+            },
+        },
+        mounted: function () {
+            document.addEventListener('scroll', this.OnScroll)
+        },
+        destroyed: function () {
+            document.removeEventListener('scroll', this.OnScroll)
+        }
+    })
 }
