f27b89f6cbdfcab5a853a9d9d7757d0dfd3e2f64
Amolition <amolbhuptani@gmail.com>
2019-07-09 10:58:29 +0100

    adding initial conversion to vue for Amol Template

---
+++ Amol Vue Version/nav_Script.js
@@ -0,0 +1,17 @@
+// Code for navigation functionality in visualisations main screen using Vue
+
+let app = new Vue ({
+
+    el: "#app",
+
+    data: {
+        test: 1,
+    },
+
+    methods: {
+    },
+
+    computed: {
+    },
+
+});
