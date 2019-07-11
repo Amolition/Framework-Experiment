daddfbf9a3aa31466e9d8958dd5422c75a5a5484
Amolition <amolbhuptani@gmail.com>
2019-07-11 10:11:22 +0100

    added buttons for section navigation to progress bar

--- Amol Vue Version/nav_Script.js
+++ Amol Vue Version/nav_Script.js
@@ -7,16 +7,16 @@ let app = new Vue ({
     data: {
         scrollPos: 0,
         currentSection: 0,
-        sectionTops: [,,,,],
-        sectionBottoms: [,,,,],
+        sectionTops: [],
+        sectionBottoms: [],
     },
 
     methods: {
         scrollFunc: function () {
 
-            let topspace =  $("#vis-title").outerHeight();
+            //let topSpace =  document.querySelectorAll("#vis-title")[0].offsetHeight;
 
-            app.scrollPos = $(".journey").scrollTop();
+            app.scrollPos = document.querySelectorAll(".journey")[0].scrollTop;
 
             console.log("scrollPos: " + app.scrollPos);
 
@@ -24,9 +24,9 @@ let app = new Vue ({
 
                 // update currentSection if user scrolls past
                 // the top edge of its corresponding paragraph on left side
-                let topSection = $("#"+"sc"+section).offset().top;
-                let bottomSection = topSection + $("#"+"sc"+section).outerHeight();
-                if (topspace >= topSection && topspace < bottomSection) {
+                let topSection = document.querySelectorAll("#"+"sc"+section)[0].offsetTop;
+                let bottomSection = topSection + document.querySelectorAll("#"+"sc"+section)[0].offsetHeight;
+                if (app.scrollPos >= topSection && app.scrollPos < bottomSection) {
                     app.currentSection = section;
                     console.log(app.currentSection);
                 };
@@ -39,27 +39,39 @@ let app = new Vue ({
 
         sectionPos: function () {
             this.$nextTick (function () {
-                let n = $(".section-container").length;
+                let n = document.querySelectorAll(".section-container").length;
+                let overallTop = document.querySelectorAll("#sc1")[0].offsetTop;
+                console.log(overallTop);
+                console.log("resized");
 
                 for (let i=1; i<=n; i++) {
 
                     if (i<n) {
-                        app.sectionTops[i-1] = ($("#"+"sc"+i).offset().top);
-                        app.sectionBottoms[i-1] = (app.sectionTops[i-1] + $("#"+"sc"+i).outerHeight())
+                        app.sectionTops[i-1] = (document.querySelectorAll("#"+"sc"+i)[0].offsetTop - overallTop);
+                        app.sectionBottoms[i-1] = (app.sectionTops[i-1] + document.querySelectorAll("#"+"sc"+i)[0].offsetHeight);
                     } else {
-                        app.sectionTops[i-1] = ($("#"+"sc"+i).offset().top);
-                        app.sectionBottoms[i-1] = (app.sectionTops[i-1] + $("#"+"sc"+i).outerHeight() - $(".journey").height())
+                        app.sectionTops[i-1] = (document.querySelectorAll("#"+"sc"+i)[0].offsetTop) - overallTop;
+                        app.sectionBottoms[i-1] = (app.sectionTops[i-1] + document.querySelectorAll("#"+"sc"+i)[0].offsetHeight - document.querySelectorAll(".journey")[0].offsetHeight);
                     }
 
                 }
             })
-        }
+        },
+
+        scrollTo: function (event) {
+            document.querySelectorAll("#"+"sc"+event.currentTarget.dataset.no)[0].scrollIntoView({behavior: "smooth"});
+        },
+
+        //resizeTest: function () {
+        //    console.log("resized")
+        //},
     },
 
     computed: {
     },
 
     mounted () {
+        //window.addEventListener('resizeTest', this.test());
         this.$nextTick( function () {
             this.sectionPos();
             this.scrollFunc();}
