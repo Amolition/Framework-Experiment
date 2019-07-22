499463ed44f565a1a874e30fde0e9142fa3843f9
Plankat <Plankat@users.noreply.github.com>
2019-07-22 11:28:34 +0100

    Eliza version

---
+++ Fourier-Series-with-style-e/Scroll_Bar_Script.js
@@ -0,0 +1,233 @@
+// Code for loading and unloading right panel as left panel is scrolled
+
+// uses jQuery .load method instead of vue to load the one appropriate right panel with respect to paragraph on left panel
+// right panels stored in separate html files which are called into main html as appropriate
+// previous right panel is unloaded by overwriting right div
+
+// import right div as jQuery object
+let loadspace = $('#loadspace');
+console.log(loadspace);
+
+// declare global variables needed
+let n=0;
+let i;
+let x=0;
+let current=0;
+
+// load first screen when web page first accessed
+loadspace.load("test_div_" + "p" + current + ".html");
+
+// calculate number of paragraphs in left div
+$("p").each(function () {
+    n++
+});
+console.log("n: "+n);
+/*$(document).ready(
+        function() {
+          $(".left").niceScroll({
+              cursorcolor: "#000000",
+              autohidemode:true,
+          });
+        }
+      );*/
+// function for creating event handlers to listen for respective paragraphs reaching (and leaving) top of screen in view
+// then load respective right panel (overwriting previous screen) when a new paragraph comes to top of screen
+// hence only one right panel loaded at a time
+$(document).scroll(function () {
+    // variable for measuring screen position with respect to scroll
+    let y = $(this).scrollTop()+ $("nav").outerHeight();
+
+    // function to return and assign number of paragraph at top of screen to x
+    function handleElement(para) {
+
+        // update x if user scrolls past
+        // the top edge of its corresponding paragraph on left side
+        let top = $("#"+"ph"+para).offset().top;
+        let bottom = top + $("#"+"lc"+para).outerHeight();
+        if (y > top && y < bottom) {
+            //$(x).fadeIn();
+            x=para;
+            console.log("para: "+para);
+            console.log('top:' + top);
+            console.log('height:' + bottom);
+            //light up the button for each section
+            //('.progress-bar').on('mouseenter', () => {
+            $("#" + "p" + para + "b").css({
+                backgroundColor: "#4afe82"
+            //})
+        })
+        } else{
+            $("#" + "p" + para + "b").css({
+                backgroundColor: "#efefef"
+        })}
+        }
+
+
+    // iteration of function for all paragraphs
+    for(i=0; i<=n; i++)
+    handleElement(i);
+
+
+    if(current!==x) {
+
+        //stick appropriate heading on left hand panel
+        if(x!==0) {
+
+            //label old heading to unstick
+            let unstick = $("#s"+(current));
+            current=x;
+            //label new heading to stick
+            let stick = $("#s"+(x));
+
+            unstick.css({
+                position: "static"
+            });
+            stick.css({
+                position: "fixed",
+                top: "12vh",
+                width: "32vw"
+            });
+        }
+        if(x===0) {
+
+            //label old heading to unstick
+            let unstick = $("#s"+(current));
+            current=x;
+
+            unstick.css({
+                position: "static"
+            });
+        }
+
+        // if x changes then old right panel changed to appropriate new right panel
+        console.log("x: "+x);
+        console.log("current: "+current);
+        $("#loadspace").hide(600, function() {loadspace.load("test_div_" + "p" + current + ".html");});
+
+        //loadspace.html("");
+        //loadspace.load("test_div_" + "p" + current + ".html");
+        $("#loadspace").fadeIn(600)//, function() {loadspace.load("test_div_" + "p" + current + ".html");});
+    }
+
+});
+
+
+$(function(){
+		$.mCustomScrollbar.defaults.scrollButtons.enable = true; //启用默认的滚动按钮
+		$(".left").mCustomScrollbar( {
+			theme : "dark"
+		});
+
+	});
+
+
+$(document).ready(() => {
+    $('#p1b').on('click',function () {
+        $('html,body').animate({scrollTop:$('#p1').offset().top - 120}, 500);
+    });
+    $('#p2b').on('click',function () {
+        $('html,body').animate({scrollTop:$('#p2').offset().top -120}, 500);
+    });
+    $('#p3b').on('click',function () {
+        $('html,body').animate({scrollTop:$('#p3').offset().top -120}, 500);
+    });
+    $('#p4b').on('click',function () {
+        $('html,body').animate({scrollTop:$('#p4').offset().top -120}, 500);
+    });
+    $('#btn_top').on('click',function () {
+        $('html,body').animate({scrollTop:0}, 500);
+    });
+    // different animation for the transition of the navigation menu
+    // hover
+    /*$('.nav-menu').on('mouseenter', () => {
+        $('.menu').removeClass('.hide');
+        $('.menu').slideDown()
+        $('.menu').on('mouseleave', () => {
+        $('.menu').fadeOut();
+        $('.menu').hide();
+    });
+
+
+    })*/
+    //click
+     $('.nav-menu').on('click', () => {
+        $('.menu').slideToggle();
+
+    });
+     // get tab name
+   $('#sc1b').on('mouseenter',() => {
+        var name = $("#n1").html();
+        $('#text').html(name);
+        $('#p1b').on('mouseleave', () => {
+            $('#text').html('hover on the section button')
+        });
+    });
+   $('#p2b').on('mouseenter',() => {
+        var name = $("#n2").html();
+        $('#text').html(name);
+        $('#p2b').on('mouseleave', () => {
+            $('#text').html('hover on the section button')
+        });
+    });
+   $('#p3b').on('mouseenter',() => {
+        var name = $("#n3").html();
+        $('#text').html(name);
+        $('#p3b').on('mouseleave', () => {
+            $('#text').html('hover on the section button')
+        });
+    });
+   $('#p4b').on('mouseenter',() => {
+        var name = $("#n4").html();
+        $('#text').html(name);
+        $('#p4b').on('mouseleave', () => {
+            $('#text').html('hover on the section button')
+        });
+    });
+});
+
+/*
+onload = function () {
+  //初始化
+  scrollToLocation();
+};
+function scrollToLocation() {
+  var mainContainer = $('.left'),
+  scrollToContainer = mainContainer.find('.:last');//滚动到<div id="thisMainPanel">中类名为son-panel的最后一个div处
+  //scrollToContainer = mainContainer.find('.son-panel:eq(5)');//滚动到<div id="thisMainPanel">中类名为son-panel的第六个处
+  //非动画效果
+  //mainContainer.scrollTop(
+  //  scrollToContainer.offset().top - mainContainer.offset().top + mainContainer.scrollTop()
+  //);
+  //动画效果
+  mainContainer.animate({
+    scrollTop: scrollToContainer.offset().top - mainContainer.offset().top + mainContainer.scrollTop()
+  }, 2000);//2秒滑动到指定位置
+}
+
+// legacy code - had issues with reloading right div for every scroll - fixed in above code
+*/
+/*
+
+});
+
+$(document).scroll(function () {
+    let y = $(this).scrollTop();
+    // Show element after user scrolls past
+    // the top edge of its parent
+    $('#p1').each(function () {
+
+        let top = $(this).offset().top;
+        let bottom = top + $(this).outerHeight();
+        if (y > top && y < bottom) {
+            //$(x).fadeIn();
+            loadspace.load("test_div_" + this.id + ".html");
+        } else {
+            //$(x).fadeOut();
+            loadspace.html("");
+        }
+        //console.log(this.id)
+
+    });
+});
+
+*/
---
+++ Fourier-Series-with-style-e/journey.js
@@ -0,0 +1,373 @@
+/* goals 2019/07/19
+* fix the scrolling problem
+* try to incorporate the transition animation between pages and sections
+* simplify coding
+* colorful loading bar
+* section name hover
+* */
+let journey_pg = document.getElementsByClassName("journey_pg");
+let progress_buttons = document.getElementsByClassName("progress_button");
+let section_titles = document.getElementsByClassName('section_title');
+let journey_cont = document.getElementById("journey_cont");
+//let journey_pgs = $(".journey_pg").height();
+let vis_pg = document.getElementsByClassName("vis_pg");
+let i;
+let scrolling;
+let pg_heights = [];
+let pg_heights2 = [];
+let h;
+
+/*import VueMathjax from 'vue-mathjax'
+    Vue.use(VueMathjax)*/
+
+
+window.onload = function () {
+
+
+    for (i = 0; i < journey_pg.length; i++) {
+        //console.log(journey_pgs)
+        pg_heights2.push($("#" + journey_pg[i].id).offset().top);
+        h = 0;
+        for (j = 0; j < (i + 1); j++) {
+            //h+=parseFloat(journey_pg[j].scrollHeight)
+            h += $("#" + journey_pg[j].id).height()
+        }
+        pg_heights.push(h);
+        console.log('p:' + pg_heights);
+        console.log('p2:'+ pg_heights2);
+        console.log('h:' + h)
+    }
+
+    /*for (i=0 ; i < pg_heights.length; i++){
+               if(){this.navBarFixed1 = true}
+    }
+    */
+    /* re-calculate the height of each section after clicking to show the hidden content */
+    $(function () {
+        function updateHeight() {
+            pg_heights=[];
+            for (i = 0; i < journey_pg.length; i++) {
+                //console.log(journey_pgs)
+                //pg_heights2.push($("#" + journey_pg[i].id).offset().top);
+                h = 0;
+                for (j = 0; j < (i + 1); j++) {
+                    //h+=parseFloat(journey_pg[j].scrollHeight)
+                    h += $("#" + journey_pg[j].id).outerHeight()
+                }
+                pg_heights.push(h);
+                console.log('newp:' + pg_heights);
+        console.log('newh:' + h)
+
+            }
+        }
+        // call the update function each time you click the button
+        $(".derivation_collapse").on('click', () => {
+            updateHeight()
+        })
+
+    });
+
+
+
+    let app = new Vue({
+        el: "#app",
+        data: {
+            pg_heights: pg_heights,
+            scrolling: 0,
+            // set the initial boolean value of the hidden contents
+            show1: false,
+            show2: false,
+            show3: false,
+            show4: false,
+            show5: false,
+            show6: false,
+            show7: false,
+            show8: false,
+            show9: false,
+            show10: false,
+            showpage1: false,
+            showpage2: false,
+            showpage3: false,
+            showpage4: false,
+            showpage5: false,
+            currentsec: 0,
+
+            
+        },
+        methods: {
+            OnScroll: function (event) {
+                this.scrolling = parseFloat($(".container_journey").scrollTop());
+
+                // need simplification
+
+                if (this.scrolling < pg_heights[0]) {
+                    app.currentsec=1;
+                    progress_buttons[0].style = "background-color: #ccffff";//; color: white;";
+                    /*progress_buttons[1].style = "background-color: white;" //"background-color: white; color: black;";
+                    progress_buttons[2].style = "background-color: white;";
+                    progress_buttons[3].style = "background-color: white;";*/
+
+                    progress_buttons[1].style = "inherit";
+                    progress_buttons[2].style = "inherit";
+                    progress_buttons[3].style = "inherit";
+                    progress_buttons[4].style = "inherit";
+                    section_titles[0].style = "position: fixed;z-index: 999; top:0; display: block";
+                    section_titles[3].style = "display: none";
+                    section_titles[1].style = "display: none";
+                    section_titles[2].style = "display: none";
+                    section_titles[4].style = "display: none";
+
+
+                } else if (this.scrolling <= pg_heights[1]) {
+                    app.currentsec=2;
+                    //progress_buttons[0].style = "background-color: white;";
+                    progress_buttons[1].style = "background-color: #ccffff"; //; color: white;";
+                    /*progress_buttons[2].style = "background-color: white;";
+                    progress_buttons[3].style = "background-color: white;";
+                    */
+                    progress_buttons[0].style = "inherit";
+                    progress_buttons[4].style = "inherit";
+                    progress_buttons[2].style = "inherit";
+                    progress_buttons[3].style = "inherit";
+                    section_titles[0].style = "display: none";
+                    section_titles[1].style = "position: fixed; top: 0; z-index: 998; display: block";
+                    section_titles[4].style = "display: none";
+                    section_titles[2].style = "display: none";
+                    section_titles[3].style = "display: none";
+                } else if (this.scrolling <= pg_heights[2]) {
+                    app.currentsec=3;
+                    /*progress_buttons[0].style = "background-color: white;";
+                    progress_buttons[1].style = "background-color: white;";
+                    */
+                    progress_buttons[2].style = "background-color: #ccffff"; //; color: white;";
+                    //progress_buttons[3].style = "background-color: white;";
+                    progress_buttons[1].style = "inherit";
+                    progress_buttons[0].style = "inherit";
+                    progress_buttons[3].style = "inherit";
+                    progress_buttons[4].style = "inherit";
+                    section_titles[0].style = "display: none";
+                    section_titles[1].style = "display: none";
+                    section_titles[3].style = "display: none";
+                    section_titles[4].style = "display: none";
+                    section_titles[2].style = "position: fixed; top: 0; z-index: 997; display: block"
+
+                } else if (this.scrolling <= pg_heights[3]) {
+                    app.currentsec=4;
+                    /*progress_buttons[0].style = "background-color: white;";
+                    progress_buttons[1].style = "background-color: white;";
+                    progress_buttons[2].style = "background-color: white;";
+                   */
+                    progress_buttons[3].style = "background-color: #ccffff"; //; color: white;";
+                    progress_buttons[1].style = "inherit";
+                    progress_buttons[2].style = "inherit";
+                    progress_buttons[0].style = "inherit";
+                    progress_buttons[4].style = "inherit";
+                    section_titles[0].style = "display: none";
+                    section_titles[1].style = "display: none";
+                    section_titles[2].style = "display: none";
+                    section_titles[4].style = "display: none";
+                    section_titles[3].style = "position: fixed; top: 0; z-index: 996; display: block"
+
+                } else if (this.scrolling <= pg_heights[4]) {
+                    app.currentsec=5;
+                    /*progress_buttons[0].style = "background-color: white;";
+                    progress_buttons[1].style = "background-color: white;";
+                    progress_buttons[2].style = "background-color: white;";
+                   */
+                    progress_buttons[4].style = "background-color: #ccffff"; //; color: white;";
+                    progress_buttons[1].style = "inherit";
+                    progress_buttons[2].style = "inherit";
+                    progress_buttons[3].style = "inherit";
+                    progress_buttons[0].style = "inherit";
+                    section_titles[0].style = "display: none";
+                    section_titles[1].style = "display: none";
+                    section_titles[2].style = "display: none";
+                    section_titles[3].style = "display: none";
+                    section_titles[4].style = "position: fixed; top: 0; z-index: 996; display: block"
+
+                }
+            },
+
+            /*OnScroll: function (event) {
+                this.scrolling = parseFloat($(".container_journey").scrollTop());
+                if (this.scrolling < pg_heights[0]) {
+                    progress_buttons[0].style = "background-color: #ccffff"*/
+
+            Scroll1: function (event) {
+                journey_pg[0].scrollIntoView({behavior: "smooth"})
+            },
+            Scroll2: function (event) {
+                journey_pg[1].scrollIntoView({behavior: "smooth"})
+            },
+            Scroll3: function (event) {
+                journey_pg[2].scrollIntoView({behavior: "smooth"})
+            },
+            Scroll4: function (event) {
+                journey_pg[3].scrollIntoView({behavior: "smooth"})
+            },
+            Scroll5: function (event) {
+                journey_pg[4].scrollIntoView({behavior: "smooth"})
+            },
+            subScrollTo: function (event) {
+            let scrollTarget = event.currentTarget;
+            setTimeout(function () {scrollTarget.scrollIntoView({behavior: "smooth"});}, 500);
+        },
+
+
+        },
+        mounted: function () {
+            document.addEventListener('scroll', this.OnScroll)
+        },
+        /*mounted: () {
+            document.addEventListener('scroll', this.watchScroll)
+        },*/
+
+        destroyed: function () {
+            document.removeEventListener('scroll', this.OnScroll)
+        },
+
+    });
+
+    /*--------------------------------------------------------------------------------------------------------------------------*/
+// loading bar
+    $(function () {
+
+        function scroll_fn() {
+
+            let heights = h - 600; //journey_pg[journey_pg.length-1]
+            //$('#main-journey').height()//-$('#main-journey').offset().top;
+
+            //var document_height = $(document).height();
+            let scroll_so_far = $('#journey_cont').scrollTop();
+
+            let scroll_percentage = scroll_so_far / (heights / 100);
+
+            //$('#loading').width(scroll_percentage + '%');
+            $('#loading').css('width', (scroll_percentage + '%'));
+            //console.log('height:'+heights);
+            //console.log('scroll:'+scroll_so_far);
+            //console.log('window_height'+window_height);
+            //console.log('percentage:'+scroll_percentage);
+            //console.log('to bottom:'+ toBottom);
+
+        }
+
+
+        $('#journey_cont').scroll(function () {
+            scroll_fn();
+        });
+
+        $(window).resize(function () {
+            scroll_fn();
+        });
+
+
+        // to show the function content selected
+
+        function showinfo() {
+            var value = document.getElementById("fourierselect").value;
+                    //show div
+            if (value==1){
+                $("#parabolic").show();
+                $("#delta").hide();
+                $("#square").hide();
+                $("#sawtooth").hide();
+                $("#modx").hide();
+            } else if (value==2){
+                $("#parabolic").hide();
+                $("#delta").show();
+                $("#square").hide();
+                $("#sawtooth").hide();
+                $("#modx").hide();
+            } else if (value==3){
+                $("#parabolic").hide();
+                $("#delta").hide();
+                $("#square").show();
+                $("#sawtooth").hide();
+                $("#modx").hide();
+            } else if (value==4){
+                $("#parabolic").hide();
+                $("#delta").hide();
+                $("#square").hide();
+                $("#sawtooth").show();
+                $("#modx").hide();
+            } else if (value==5){
+                $("#parabolic").hide();
+                $("#delta").hide();
+                $("#square").hide();
+                $("#sawtooth").hide();
+                $("#modx").show();
+            }
+
+        }
+    //console.log('executed')
+        $("#fourierselect").on('click',() => {
+            showinfo()
+        });
+
+
+
+    });
+};
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/*
+// welcome loading
+
+$("#welcome").html("60秒后重新获取");
+		var count = 60;
+
+		var timeInterval = setInterval(function() {
+			if (count > 1) {
+				count--;
+				$("#getVerifyCode").html(count + "秒后重新获取");
+
+			} else {
+				$("#getVerifyCode").html("获取验证码");
+				//设置停止
+				clearInterval(timeInterval);
+			}
+
+		}, 1000);
+*/
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
+/*
+
+window.addEventListener('load', function() {
+  MathJax.Hub.Queue(
+    ["setRenderer",MathJax.Hub,"SVG"]
+  );
+  Plotly.d3.json('/{{https://cdn.plot.ly/plotly-latest.min.js}}', function(error, fig) {
+    Plotly.plot('{{graph}}', fig.data, fig.layout);
+  MathJax.Hub.Queue(
+    ["setRenderer",MathJax.Hub,"HTMLCSS-output"]
+  );
+  });
+});
+*/
+
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+/*$(document).ready(() => {
+    $('#derive').on('click', () => {
+        $('#derive-content').slideToggle();
+    })
+})*/
+
+/*
+
+$(document).ready(() => {
+    $('.menu').on('click', () => {
+        $('.menuContent').slideToggle();
+    });
+    console.log('shuru')
+});*/
+/*
+
+new Vue({
+  el: '.menu',
+  data: {
+    show: false
+  }
+})*/
