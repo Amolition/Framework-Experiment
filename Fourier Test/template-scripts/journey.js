0493df3de589f530b0f07c3f75f21888b4771702
Antoine Vauterin <av5218@ic.ac.uk>
2019-07-25 12:33:55 +0100

    Antoine's version, added SPF

--- Fourier Test/template-scripts/journey.js
+++ Fourier Test/template-scripts/journey.js
@@ -15,6 +15,7 @@ let n = 0;
 
 
 
+
 //When the page loads this sets all the buttons to inactive and calculates the height of each section.
 window.onload = function () {
     progress_buttons[0].style = "background-color: #003e74; color: white;"
@@ -36,7 +37,7 @@ window.onload = function () {
             pg_heights: pg_heights,
             scrolling: 0, //position of the scrollbar
             isActive: {   //true for any active collapsable box
-                'derivation_regroup': false, 
+                'derivation_regroup': false,
                 'triangular': false,
                 'triangular_byParts': false,
                 'parabola': false,
@@ -50,57 +51,67 @@ window.onload = function () {
                 'mod': false,
                 'mod_byParts': false,
             },
+            subVis: {
+                'triangular': false,
+                'parabola': false,
+                'dirac': false,
+                'square': false,
+                'sawtooth': false,
+                'mod': false,
+            },
+            boolean: false,
+            temporary_boolean: false,
             current: 1,  //keeps track of the current section
         },
         methods: {
             OnScroll: function (event) {  //method called onScroll to check the section heights 
-                                          //and change the colors of the buttons according to the position
-                                          //on the page
+                //and change the colors of the buttons according to the position
+                //on the page
                 this.scrolling = parseFloat($(".container_journey").scrollTop());
                 if (this.scrolling < this.pg_heights[0]) {
-                    for (i=0;i<n;i++) {
-                        if (i===0) {
+                    for (i = 0; i < n; i++) {
+                        if (i === 0) {
                             progress_buttons[i].style = "background-color: #003e74; color: white;";
                         } else {
                             progress_buttons[i].style = "background-color: white; color: black;";
                         }
                     };
-                    if (this.current!==1) {  //calls the function that draws the plotly graph for this section
+                    if (this.current !== 1  && typeof(document.getElementById('graph'))!='undefined' && document.getElementById('graph')!=null) {  //calls the function that draws the plotly graph for this section
                         main_0();
                         this.current = 1;
                     }
-                } else if (this.scrolling<this.pg_heights[1]) {
-                    for (i=0;i<n;i++) {
-                        if (i===1) {
+                } else if (this.scrolling < this.pg_heights[1]) {
+                    for (i = 0; i < n; i++) {
+                        if (i === 1) {
                             progress_buttons[i].style = "background-color: #003e74; color: white;";
                         } else {
                             progress_buttons[i].style = "background-color: white; color: black;";
                         }
                     };
-                    if (this.current!==2) {
+                    if (this.current !== 2 && typeof(document.getElementById('graph3'))!='undefined' && document.getElementById('graph3')!=null) {
                         main_1();
                         this.current = 2;
                     }
-                } else if (this.scrolling<this.pg_heights[2]) {
-                    for (i=0;i<n;i++) {
-                        if (i===2) {
+                } else if (this.scrolling < this.pg_heights[2]) {
+                    for (i = 0; i < n; i++) {
+                        if (i === 2) {
                             progress_buttons[i].style = "background-color: #003e74; color: white;";
                         } else {
                             progress_buttons[i].style = "background-color: white; color: black;";
                         }
                     };
-                    if (this.current!==3) {
+                    if (this.current !== 3 && !this.boolean) {
                         this.current = 3;
                     }
-                } else if (this.scrolling<this.pg_heights[3]) {
-                    for (i=0;i<n;i++) {
-                        if (i===3) {
+                } else if (this.scrolling < this.pg_heights[3]) {
+                    for (i = 0; i < n; i++) {
+                        if (i === 3) {
                             progress_buttons[i].style = "background-color: #003e74; color: white;";
                         } else {
                             progress_buttons[i].style = "background-color: white; color: black;";
                         }
                     }
-                    if (this.current!==4) {
+                    if (this.current !== 4 && !this.boolean) {
                         this.current = 4;
                     }
                 }
@@ -114,24 +125,50 @@ window.onload = function () {
                     }
                     this.pg_heights.push(h)
                 };
-                
+                this.temporary_boolean = false;
+                for (let key in this.subVis) {
+
+                    if (this.isActive[key]) {
+                        topk = document.getElementById(key).offsetTop;
+                        height = $("#" + key).outerHeight();
+                        //console.log(topk);
+                        //console.log(topk + height);
+                        if (this.scrolling > topk && this.scrolling < topk + height) {
+                            this.subVis[key] = true
+                            //console.log('hello');
+                            this.temporary_boolean = true;
+                            //console.log(this.temporary_boolean);
+                            if (this.current !== key && typeof(document.getElementById('graph_subVis'))!='undefined' && document.getElementById('graph_subVis')!=null) {
+                                main_3(key);
+                                this.current = key;
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                this.boolean = this.temporary_boolean
+
+
+
             },
             OnClick: function (section) {  //function to toggle collapsable divs on click
-                this.isActive[section] = !this.isActive[section]   
+                this.isActive[section] = !this.isActive[section];
+                //alert(this.boolean)
             },
             Scroll: function (numb) {  //scroll to the right section
-                journey_pg[numb-1].scrollIntoView({behavior:'smooth'})
+                journey_pg[numb - 1].scrollIntoView({ behavior: 'smooth' })
             },
         },
         mounted: function () {
             document.addEventListener('scroll', this.OnScroll);
-            MathJax.Hub.Queue(["Typeset",MathJax.Hub,"app"]); //reloads mathjax 
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, "app"]); //reloads mathjax 
         },
         destroyed: function () {
             document.removeEventListener('scroll', this.OnScroll)
         }
     })
 }
-
 
 
